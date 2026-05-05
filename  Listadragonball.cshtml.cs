069f2903ae960730c1b7efd2128923bab5090578
Frederico994 <a14620@oficina.pt>
2026-05-05 15:13:34 +0000

    projeto

---  Listadragonball.cshtml.cs
+++  Listadragonball.cshtml.cs
@@ -2,14 +2,46 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TesteCsarp.Pages
-
-
 {
+
+
     public class  ListadragonballModel : PageModel
     {
-        public void OnGet()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public IndexModel(IHttpClientFactory httpClientFactory)
+
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public List<Listadragonball> Listadragonball { get; set; } = new();
+    public async Task OnGetAsync()
+
+    {
+        //var client = _httpClientFactory.CreateClient();
+        //var response = await client.GetAsync("https://dragonball-api.com/api/characters/2");
+        var client = _httpClientFactory.CreateClient("RestCountries");
+        var response = await client.GetAsync("https://dragonball-api.com/api/characters");
+
+        if (response.IsSuccessStatusCode)
+
         {
-            
+            var json = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var dados = JsonSerializer.Deserialize<List<CountryApiResponse>>(json, options);
+
+            Listadragonball = dados.Select(d => new Listadragonball
+            {
+                OfficialName = d.name?.official,
+                Cca2 = d.cca2,
+                FlagUrl = d.flags?.png
+            }).ToList();
         }
     }
+
+
 }
+}    
+    
+
